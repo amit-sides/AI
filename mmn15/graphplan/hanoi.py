a7666b846d9b250b4c09c7ca91c7f9f89a20f6e7
Amit Sides <amit794@walla.com>
2020-12-26 23:27:17 +0200

    Q13-Q14 done

--- mmn15/graphplan/hanoi.py
+++ mmn15/graphplan/hanoi.py
@@ -1,18 +1,60 @@
+def get_third_peg(pegs, src_peg, dest_peg):
+  for peg in pegs:
+    if peg == src_peg:
+      continue
+    if peg == dest_peg:
+      continue
+    return peg
+  return None
+
 def createDomainFile(domainFileName, n):
   numbers = list(range(n)) # [0,...,n-1]
   pegs = ['a','b', 'c']
   domainFile = open(domainFileName, 'w') #use domainFile.write(str) to write to domainFile
-  "*** YOUR CODE HERE ***"
-  
+
+  # Writes propositions
+  domainFile.write("Propositions:\n")
+  for number in numbers:
+    for peg in pegs:
+      prop = "{}{} ".format(peg, number)
+      domainFile.write(prop)
+
+  # Writes actions
+  domainFile.write("\nActions:\n")
+  for number in numbers:
+    for src_peg in pegs:
+      for dest_peg in pegs:
+        if src_peg == dest_peg:
+          continue
+        name = "Name: M{}{}{}".format(number, src_peg, dest_peg)
+        third_peg = get_third_peg(pegs, src_peg, dest_peg)
+        preconditions = ["{}{}".format(third_peg, i) for i in range(number)]
+        pre = "pre: {}{} {}".format(src_peg, number, " ".join(preconditions))
+        add = "add: {}{}".format(dest_peg, number)
+        delete = "delete: {}{}".format(src_peg, number)
+        domainFile.write(name+"\n")
+        domainFile.write(pre + "\n")
+        domainFile.write(add + "\n")
+        domainFile.write(delete + "\n")
+
   domainFile.close()  
-        
-  
+
 def createProblemFile(problemFileName, n):
   numbers = list(range(n)) # [0,...,n-1]
   pegs = ['a','b', 'c']
   problemFile = open(problemFileName, 'w') #use problemFile.write(str) to write to problemFile
-  "*** YOUR CODE HERE ***"
-  
+
+  # Writes initial state
+  problemFile.write("Initial state: ")
+  states = ["{}{}".format(pegs[0], i) for i in numbers]
+  problemFile.write(" ".join(states))
+
+  # Writes goal
+  problemFile.write("\nGoal state: ")
+  states = ["{}{}".format(pegs[-1], i) for i in numbers]
+  problemFile.write(" ".join(states))
+
+  problemFile.write("\n")
   problemFile.close()
 
 import sys
